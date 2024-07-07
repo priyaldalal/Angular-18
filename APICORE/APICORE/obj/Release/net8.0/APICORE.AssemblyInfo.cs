af399cbb325869e39132ef44b7f29dcfee5d6a7f
Priyal Dalal <84765716+priyaldalal@users.noreply.github.com>
2024-07-07 14:50:34 +0530

    api added
    api addde

--- APICORE/APICORE/obj/Release/net8.0/APICORE.AssemblyInfo.cs
+++ APICORE/APICORE/obj/Release/net8.0/APICORE.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICORE")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e57cb50946e21a33cd2ecafa2ade4c7d413173d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICORE")]
